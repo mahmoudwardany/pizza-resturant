8dce1389a8bb59e3b5e11c3399252e29f26cf3fe
mahmoud <mahmoudwardany18@gmail.com>
2023-04-02 16:03:24 +0200

    final

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -74,36 +74,35 @@ const handleStatus=async(id)=>{
         ))}
       </table>
     </div>
-    <div className='grid col-md-5 col-sm-12 text-center'>
+    <div className='grid col-md-5 col-sm-10 text-center'>
       <h1 className={styles.title}>Orders</h1>
-      <table className='table-bordered p-20 col-md-12 text-center'>
+      <table className='col-md-12 '>
         <tbody>
           <tr className={styles.trTitle}>
             <th>Id</th>
             <th>Customer</th>
             <th>Total</th>
-            <th>Payment</th>
             <th>Status</th>
             <th>Action</th>
           </tr>
         </tbody>
         {orderList.map((order) => (
           <tbody key={order._id}>
-            <tr className={styles.trTitle}>
-              <td>{order._id.slice(0, 5)}...</td>
-              <td>{order.customer}</td>
-              <td>${order.total}</td>
-              <td>
-                {order.method === 0 ? <span>cash</span> : <span>paid</span>}
-              </td>
-              <td>{status[order.status]}</td>
-              <td>
-                <button onClick={() => handleStatus(order._id)} className='btn btn-info'>
-                  Next Stage
-                </button>
-              </td>
-            </tr>
-          </tbody>
+          <tr className={styles.trTitle}>
+            <td>{order._id.slice(0, 5)}...</td>
+            <td>{order.customer}</td>
+            <td>${order.total}</td>
+            <td>
+              {order.method === 0 ? <span>cash</span> : <span>paid</span>}
+            </td>
+            <td>{status[order.status]}</td>
+            <td>
+              <button onClick={() => handleStatus(order._id)} className='btn btn-info'>
+                Next Stage
+              </button>
+            </td>
+          </tr>
+        </tbody>
         ))}
       </table>
     </div>
