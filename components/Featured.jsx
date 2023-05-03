351a98b23f1fe3036baa1dd113ad1a57c0fbf7a0
mahmoud <mahmoudwardany18@gmail.com>
2023-05-03 07:54:31 +0300

    final

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from '../styles/Featured.module.css'
-
 const Featured = () => {
   return (
- <div id={styles.wrapper} className='w-100'>
-<Image  src={'/img/pizza-red-flyer_1284-34366.avif'}
-width={500}height={200}
-className='min-w-full min-h-fit'
+ <div id={styles.wrapper} className='h-50'>
+<Image  src={'/img/side-view-pizza-with-slices-bell-pepper-pizza-slices-flour-board-cookware_176474-3185.avif'}
+width={500}height={100}
+className='w-full max-h-full'
 alt=''
 />
     </div>
